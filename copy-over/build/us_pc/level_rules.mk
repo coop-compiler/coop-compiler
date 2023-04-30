8ab62d91c4462e35a79b33db32ed182c935e5d7b
David Joslin <joslin_productions@hotmail.com>
2023-04-29 18:21:46 -0700

    Update copy-over

---
+++ copy-over/build/us_pc/level_rules.mk
@@ -0,0 +1,38 @@
+
+
+
+$(eval $(call level_rules,bbh,spooky))
+$(eval $(call level_rules,ccm,snow))
+$(eval $(call level_rules,castle_inside,inside))
+$(eval $(call level_rules,hmc,cave))
+$(eval $(call level_rules,ssl,generic))
+$(eval $(call level_rules,bob,generic))
+$(eval $(call level_rules,sl,snow))
+$(eval $(call level_rules,wdw,grass))
+$(eval $(call level_rules,jrb,water))
+$(eval $(call level_rules,thi,grass))
+$(eval $(call level_rules,ttc,machine))
+$(eval $(call level_rules,rr,sky))
+$(eval $(call level_rules,castle_grounds,outside))
+$(eval $(call level_rules,bitdw,sky))
+$(eval $(call level_rules,vcutm,outside))
+$(eval $(call level_rules,bitfs,sky))
+$(eval $(call level_rules,sa,inside))
+$(eval $(call level_rules,bits,sky))
+$(eval $(call level_rules,lll,fire))
+$(eval $(call level_rules,ddd,water))
+$(eval $(call level_rules,wf,grass))
+$(eval $(call level_rules,ending,generic))
+$(eval $(call level_rules,castle_courtyard,outside))
+$(eval $(call level_rules,pss,mountain))
+$(eval $(call level_rules,cotmc,cave))
+$(eval $(call level_rules,totwc,sky))
+$(eval $(call level_rules,bowser_1,generic))
+$(eval $(call level_rules,wmotr,generic))
+
+$(eval $(call level_rules,bowser_2,fire))
+$(eval $(call level_rules,bowser_3,generic))
+
+$(eval $(call level_rules,ttm,mountain))
+
+
